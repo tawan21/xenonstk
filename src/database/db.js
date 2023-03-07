4cc1d88dec1759abd216d10430acb5f592f8a40f
Tawanjot Singh <tawanjot_singh@yahoo.com>
2023-03-07 18:11:34 +0530

    security fix

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://tawanjsingh:08a8nVtsZehVODI9@memorial-clg.i52zxmp.mongodb.net/memclg?retryWrites=true&w=majority';
+const mongoURI = process.env.URI;
 const connectToMongo = async () => {
   const res = await mongoose.connect(mongoURI);
   console.log(res)
